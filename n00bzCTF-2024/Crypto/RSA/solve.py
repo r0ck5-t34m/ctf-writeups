d1ef95ce5531cd051eb22ee50842319b590c32b4
hex01e <houmanso@student.1337.ma>
2024-08-06 23:56:34 +0100

    ..

--- n00bzCTF-2024/Crypto/RSA/solve.py
+++ n00bzCTF-2024/Crypto/RSA/solve.py
@@ -2,9 +2,8 @@ from Crypto.Util.number import long_to_bytes
 import gmpy2
 
 f = open("encryption.txt")
-
 e = int(f.readline().split()[-1])
-n = int(f.readline().split()[-1])
+n = int(f.readline().split()[-1]) 
 c = int(f.readline().split()[-1])
 
 m, _ = gmpy2.iroot(c, e)
